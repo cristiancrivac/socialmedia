feb86acd322cb2c462bb55ef318420aaddd01fc2
Cristian Crivac <ccristian.crivac@gmail.com>
2022-10-15 14:54:20 -0700

    Updated controller endpoint

--- src/main/java/com/example/socialmedia/user/UserController.java
+++ src/main/java/com/example/socialmedia/user/UserController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "/user")
+@RequestMapping(path = "/api/v1/user")
 public class UserController {
 
     private final UserService userService;
